51014eec6c439c8068b9dd011d6715025f218837
Inga A <inga.alyakskina@gmail.com>
2022-07-10 14:11:49 -0400

    HW3 update

--- HW3_Sign_In_Amazon.py
+++ HW3_Sign_In_Amazon.py
@@ -14,10 +14,8 @@ driver.find_element(By.CSS_SELECTOR, 'a.nav-a.nav-a-2#nav-orders span.nav-line-2
 expected_text = 'Sign-In'
 actual_text = driver.find_element(By.CSS_SELECTOR, "h1.a-spacing-small").text
 assert actual_text == expected_text, f'Expected {expected_text} but got {actual_text}'
-#driver.find_element(By.XPATH, '//*[@id="authportal-main-section"]//h1')
 
 #verify field e-mail is present
 assert driver.find_element(By.CSS_SELECTOR, '#ap_email').is_displayed()
-
 
 driver.quit()
